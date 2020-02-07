5c13abbe8fcd95928a880684f9e5d1ba01484c91
Nalin Ahuja <42979014+nalinahuja22@users.noreply.github.com>
2020-02-07 15:41:24 -0500

    Create MinMax.java

---
+++ MinMax.java
@@ -0,0 +1,25 @@
+public class MinMax {
+  public static int[] getMinMax(int arr[]) {
+    return divide(arr, 0, arr.length - 1);
+  }
+
+  public static int[] divide(int arr[], int l, int r) {
+    if ((r - l) <= 1) {
+      int lmin = arr[l];
+      int lmax = arr[r];
+
+      if (lmin > lmax) {
+        lmin = lmin + lmax;
+        lmax = lmin - lmax;
+        lmin = lmin - lmax;
+      }
+
+      return (new int[]{lmin, lmax});
+    } else {
+      int lmm[] = divide(arr, (l + r) / 2 + 1, r);
+      int rmm[] = divide(arr, l, (l + r) / 2);
+
+      return (new int[]{Math.min(lmm[0], rmm[0]), Math.max(lmm[1], rmm[1])});
+    }
+  }
+}
